5d10e0e222129c65287d0d72dc72c19c1944c834
JhonCode <jhon.380172793@ucaldas.edu.co>
2023-02-20 18:33:19 -0500

    Alerts login and register form done with firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Helmet } from "react-helmet";
 import Boton from './elements/Boton';
 import { ContenedorBoton, Formulario, Input } from './elements/ElementsOfForm';
 import { ContenedorHeader, Header, Titulo } from './elements/Header';
 import { ReactComponent as SvgLogin } from '../images/login.svg';
 import styled from 'styled-components';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase/firebaseConfig';
+import Alert from './elements/Alert';
 
 const Svg = styled(SvgLogin)`
   width: 100%;
@@ -13,10 +17,68 @@ const Svg = styled(SvgLogin)`
 `;
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alertState, setAlertState] = useState(false);
+  const [alert, setAlert] = useState({});
 
-  const login = (e) => {
+  const hadleChange = (e) => {
+
+    if (e.target.name === 'email') {
+      setEmail(e.target.value);
+    } else {
+      setPassword(e.target.value);
+    }
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('login user...');
+    setAlertState(false);
+    setAlert({});
+
+    if (email === '' || password === '') {
+      setAlertState(true);
+      setAlert({
+        type: 'error',
+        message: 'Todos los campos son obligatorios'
+      });
+      return;
+    }
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      setAlertState(true);
+      setAlert({
+        type: 'success',
+        message: 'La cuenta se creó correctamente'
+      });
+      navigate('/');
+
+    } catch (error) {
+      setAlertState(true);
+
+      let message;
+      switch (error.code) {
+        case 'auth/user-not-found':
+          message = 'No existe una cuenta con ese email';
+          break;
+        case 'auth/invalid-email':
+          message = 'El email no es válido o no existe';
+          break;
+        case 'auth/wrong-password':
+          message = 'La contraseña no es válida';
+          break;
+        default:
+          message = 'Hubo un error al intentar crear la cuenta';
+          break;
+      }
+
+      setAlert({
+        type: 'error',
+        message: message
+      });
+    }
   }
 
   return (
@@ -34,24 +96,33 @@ const Login = () => {
         </ContenedorHeader>
       </Header>
 
-      <Formulario onSubmit={login}>
+      <Formulario onSubmit={handleSubmit}>
         <Svg />
         <Input
           type="email"
           name="email"
           placeholder="Email"
-          required
+          value={email}
+          onChange={hadleChange}
         />
         <Input
           type="password"
           name="password"
           placeholder="Password"
-          required
+          value={password}
+          onChange={hadleChange}
         />
         <ContenedorBoton>
           <Boton as="button" primario type="submit">Login</Boton>
         </ContenedorBoton>
       </Formulario>
+
+      <Alert
+        type={alert.type}
+        message={alert.message}
+        alertState={alertState}
+        changeAlertState={setAlertState}
+      />
     </>
   );
 }
